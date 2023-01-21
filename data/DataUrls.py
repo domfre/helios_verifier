dca6d0f6265c36430b31038a1d348b009b13536a
Dominik Frey <dominik.frey13@gmail.com>
2023-01-21 10:10:40 +0100

    move repo to parent directory

--- data/DataUrls.py
+++
@@ -1,8 +0,0 @@
-ELECTION_BASE_URL = 'https://vote.heliosvoting.org/helios/elections/'
-VOTERS_BASE_URL_WITH_ELECTION_PLACEHOLDER = 'https://vote.heliosvoting.org/helios/elections/election_uuid/voters'
-RESULTS_BASE_URL_WITH_ELECTION_PLACEHOLDER = 'https://vote.heliosvoting.org/helios/elections/election_uuid/result'
-BALLOTS_BASE_URL_WITH_ELECTION_AND_VOTER_PLACEHOLDERS = 'https://vote.heliosvoting.org/helios/elections/election_uuid' \
-                                                        '/ballots/voter_uuid/last'
-TRUSTEES_BASE_URL_WITH_ELECTION_PLACEHOLDER = 'https://vote.heliosvoting.org/helios/elections/election_uuid/trustees'
-AUDITED_BALLOT_WITH_ELECTION_PLACEHOLDER = 'https://vote.heliosvoting.org/helios/elections/election_uuid' \
-                                           '/audited-ballots/?vote_hash=vote_hash_placeholder'
